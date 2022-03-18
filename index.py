1e5c914725b611ff94b3e6d58474ec5a25e216c0
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-03-18 18:49:41 +0530

    Added Few Small Scrappers

--- index.py
+++ index.py
@@ -1,10 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
+# import re
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 number = '0123456789'
 
-# dictionary.com
-def get_letter_dictionary_com(letter):
+# https://www.dictionary.com/
+def get_letter_dictionary(letter) -> list:
 	url = f"https://api-portal.dictionary.com/dcom/list/{letter}?limit=100000"
 	response = requests.get(url)
 	print("Server Response Code for char %s: %s" % (letter, response.status_code))
@@ -12,20 +14,105 @@ def get_letter_dictionary_com(letter):
 	words = [word["displayForm"] for word in json["data"]]
 	return words
 
-def scrap_dictionary_com():
+def scrap_dictionary() -> list:
 	word_list = []
-	responses = [get_letter_dictionary_com(char) for char in alphabet]
+	responses = [get_letter_dictionary(char) for char in alphabet]
 	for res in responses:
 		word_list.extend(res)
 	return word_list
 
-def save_dictionary_com():
-	words = scrap_dictionary_com()
+def save_dictionary():
+	words = scrap_dictionary()
 	with open("words/dictonary.txt","w",encoding="utf-8") as file:
 		file.write('\n'.join(words))
 
+# https://www.yourdictionary.com/
+def get_letter_yourdictionary(letter) -> list:
+	url = f"https://www.yourdictionary.com/index/{letter}"
+	response = requests.get(url)
+	print("Server Response Code for char %s: %s" % (letter, response.status_code))
+	soup = BeautifulSoup(response.text, 'html.parser')
+	word_elems = soup.find(class_='examples-list').find_all('a')
+	words = [word.text.replace("\n", "").strip() for word in word_elems]
+	# print(words)
+	return words
+
+def scrap_yourdictionary() -> list:
+	word_list = []
+	responses = [get_letter_yourdictionary(char) for char in alphabet]
+	for res in responses:
+		word_list.extend(res)
+	return word_list
+
+def save_yourdictionary():
+	words = scrap_yourdictionary()
+	with open("words/yourdictionary.txt","w",encoding="utf-8") as file:
+		file.write('\n'.join(words))
+
+# https://www.merriam-webster.com/dictionary/
+def get_letter_merriam(letter) -> list:
+	url = f"https://www.merriam-webster.com/dictionary/{letter}"
+	response = requests.get(url)
+	print("Server Response Code for char %s: %s" % (letter, response.status_code))
+	soup = BeautifulSoup(response.text, 'html.parser')
+	word_elems = soup.find_all(class_='entry-word')
+	words = [word.text.replace("\n", "").strip() for word in word_elems]
+	# print(words)
+	return words
+
+def scrap_merriam() -> list:
+	word_list = []
+	responses = [get_letter_merriam(char) for char in alphabet]
+	for res in responses:
+		word_list.extend(res)
+	return word_list
+
+def save_merriam():
+	words = scrap_merriam()
+	with open("words/merriam.txt","w",encoding="utf-8") as file:
+		file.write('\n'.join(words))
+
+# https://www.oxforddictionaries.com/
+
+def scrap_oxford() -> list:
+	session = requests.Session()
+	response = session.get('https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000/')
+	soup = BeautifulSoup(response.text, 'html.parser')
+	return [word.text.replace("\n", "").strip() for word in soup.find(class_='top-g').find_all('a')]
+
+def save_oxford():
+	words = scrap_oxford()
+	with open("words/oxford.txt","w",encoding="utf-8") as file:
+		file.write('\n'.join(words))
+
+# https://www.macmillandictionary.com/
+def get_letter_macmillan(letter) -> list:
+	url = f"https://www.macmillandictionary.com/dictionary/british/{letter}"
+	response = requests.get(url)
+	print("Server Response Code for char %s: %s" % (letter, response.status_code))
+	soup = BeautifulSoup(response.text, 'html.parser')
+	word_elems = soup.find_all(class_='hw')
+	words = [word.text.replace("\n", "").strip() for word in word_elems]
+	# print(words)
+	return words
+
+def scrap_macmillan() -> list:
+	word_list = []
+	responses = [get_letter_macmillan(char) for char in alphabet]
+	for res in responses:
+		word_list.extend(res)
+	return word_list
+
+def save_macmillan():
+	words = scrap_macmillan()
+	with open("words/macmillan.txt","w",encoding="utf-8") as file:
+		file.write('\n'.join(words))
+
 def main():
-	save_dictionary_com()
+	# save_oxford() # Not Working
+	# save_macmillan() # Not Working (Same Reason as Oxford)
+	save_yourdictionary()
+	save_dictionary()
 
 if __name__ == '__main__':
 	main()
