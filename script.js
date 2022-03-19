e786814db5c38ba2ef74aeac17738a0bcc0fb9f7
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-03-19 16:35:33 +0530

    proper support for tabbing

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ document.querySelectorAll("#current_info > input").forEach((elem) => {
 	elem.addEventListener("keydown", (e) => {
 		e.preventDefault();
 		console.log(e, e.keyCode);
-		if (match(e, "ArrowRight", 39) || match(e, "ArrowDown", 40) || match(e, "Tab", 9)) {
+		if (
+			match(e, "ArrowRight", 39) ||
+			match(e, "ArrowDown", 40) ||
+			(match(e, "Tab", 9) && !e.shiftKey)
+		) {
 			elem.nextElementSibling?.focus();
-		} else if (match(e, "ArrowLeft", 37) || match(e, "ArrowUp", 38)) {
+		} else if (
+			match(e, "ArrowLeft", 37) ||
+			match(e, "ArrowUp", 38) ||
+			(match(e, "Tab", 9) && e.shiftKey)
+		) {
 			elem.previousElementSibling?.focus();
 		} else if (match(e, "Backspace", 8)) {
 			elem.previousElementSibling?.focus();
