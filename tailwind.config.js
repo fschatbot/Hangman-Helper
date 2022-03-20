5fa1db257ad539ed441e221137b1f21d5979de5d
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-03-20 08:36:48 +0530

    Added tailwind forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: { extend: {} },
-	plugins: [require("ps-scrollbar-tailwind")],
+	plugins: [require("@tailwindcss/forms"), require("ps-scrollbar-tailwind")],
 };
